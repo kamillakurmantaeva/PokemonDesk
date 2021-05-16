c4cbbf2bd5aa55b922f4506413600542c0371fba
Kamilla <kamilla.danielevna@gmail.com>
2021-05-16 21:48:10 +0300

    created HomePage

--- src/example.ts
+++ src/example.ts
@@ -1,33 +1,33 @@
-// Работа с простыми типами
-// Напиши тип функции, конкатенирующей две строки
+// // Работа с простыми типами
+// // Напиши тип функции, конкатенирующей две строки
 
-function concat(str1: string, str2: string): string {
-  return str1 + str2;
-}
+// function concat(str1: string, str2: string): string {
+//   return str1 + str2;
+// }
 
-concat('Hello ', 'World'); // -> Hello World;
+// concat('Hello ', 'World'); // -> Hello World;
 
-// Работа с интерфейсами
-// Напиши интерфейс для описания следующих данных
+// // Работа с интерфейсами
+// // Напиши интерфейс для описания следующих данных
 
-interface homeTaskInterface {
-  howIDoIt: string;
-  simeArray: Array<string | number>;
-  withData: Array<object>;
-}
+// interface homeTaskInterface {
+//   howIDoIt: string;
+//   simeArray: Array<string | number>;
+//   withData: Array<object>;
+// }
 
-const myHometask: homeTaskInterface = {
-  howIDoIt: 'I Do It Wel',
-  simeArray: ['string one', 'string two', 42],
-  withData: [{ howIDoIt: 'I Do It Wel', simeArray: ['string one', 23] }],
-};
+// const myHometask: homeTaskInterface = {
+//   howIDoIt: 'I Do It Wel',
+//   simeArray: ['string one', 'string two', 42],
+//   withData: [{ howIDoIt: 'I Do It Wel', simeArray: ['string one', 23] }],
+// };
 
-// Типизация функций, используя Generic
-// В уроке про Generics мы написали интерфейс массива MyArray...
-// Добавь типизацию для метода reduce
+// // Типизация функций, используя Generic
+// // В уроке про Generics мы написали интерфейс массива MyArray...
+// // Добавь типизацию для метода reduce
 
-interface MyArray<T> {
-  [N: number]: T;
+// interface MyArray<T> {
+//   [N: number]: T;
 
-  reduce<U>(fn: (acc: U, el: T, i: number, arr: T[]) => U, arg: U): U;
-}
+//   reduce<U>(fn: (acc: U, el: T, i: number, arr: T[]) => U, arg: U): U;
+// }
